abd217093c5bdbeb0909dea3759fbce13649fda6
Isaac Ndubuisi <theisaac3n@gmail.com>
2023-06-22 06:27:25 +0100

    job details page completed along with mobile app linking

--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import {
+	View,
+	Text,
+	TouchableOpacity,
+	Image,
+	Linking,
+	Touchable,
+} from "react-native";
 
-import styles from './footer.style'
+import styles from "./footer.style";
 
-const Footer = () => {
-  return (
-    <View>
-      <Text>Footer</Text>
-    </View>
-  )
-}
+import { icons } from "../../../constants";
 
-export default Footer
+const Footer = ({ url }) => {
+	return (
+		<View style={styles.container}>
+			<TouchableOpacity style={styles.likeBtn}>
+				<Image
+					source={icons.heartOutline}
+					resizedMode="contain"
+					style={styles.likeBtnImage}
+				/>
+			</TouchableOpacity>
+
+			<TouchableOpacity
+				style={styles.applyBtn}
+				onPress={() => Linking.openURL(url)}
+			>
+				<Text style={styles.applyBtnText}>Apply for job</Text>
+			</TouchableOpacity>
+		</View>
+	);
+};
+
+export default Footer;
